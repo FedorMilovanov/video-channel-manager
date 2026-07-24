cb72733a8facbd8208ce596990e1ce32bcf69cc5
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-07-25 00:13:54 +0300

    Add YouTube copy validation package

---
+++ src/video_channel_manager/editorial/__init__.py
@@ -0,0 +1,5 @@
+"""Deterministic editorial checks for platform copy."""
+
+from video_channel_manager.editorial.youtube_copy import CopyFinding, validate_youtube_description
+
+__all__ = ["CopyFinding", "validate_youtube_description"]
