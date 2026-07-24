2838d9af4e96f90fa6dd10e4129dc3602642cccd
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-07-25 01:40:50 +0300

    Export guarded YouTube writer

--- src/video_channel_manager/platforms/youtube/__init__.py
+++ src/video_channel_manager/platforms/youtube/__init__.py
@@ -1,9 +1,21 @@
 from video_channel_manager.platforms.youtube.adapter import YouTubeAdapter
 from video_channel_manager.platforms.youtube.client import YouTubeApiClient, YouTubeApiError
 from video_channel_manager.platforms.youtube.models import ChannelIdentity, InstalledClientConfig, OAuthToken, YouTubeAccount
-from video_channel_manager.platforms.youtube.oauth import InstalledOAuthFlow, OAuthFlowError, YOUTUBE_READONLY_SCOPE
+from video_channel_manager.platforms.youtube.oauth import (
+    InstalledOAuthFlow,
+    OAuthFlowError,
+    YOUTUBE_FORCE_SSL_SCOPE,
+    YOUTUBE_READONLY_SCOPE,
+)
 from video_channel_manager.platforms.youtube.service import YouTubeInventoryService
 from video_channel_manager.platforms.youtube.store import AccountNotFoundError, TokenStore
+from video_channel_manager.platforms.youtube.writer import (
+    VideoDescriptionSnapshot,
+    YouTubeDescriptionWriter,
+    YouTubeRevisionConflictError,
+    YouTubeWriteError,
+    YouTubeWriteScopeError,
+)
 
 __all__ = [
     "AccountNotFoundError",
@@ -13,10 +25,16 @@ __all__ = [
     "OAuthFlowError",
     "OAuthToken",
     "TokenStore",
+    "VideoDescriptionSnapshot",
+    "YOUTUBE_FORCE_SSL_SCOPE",
     "YOUTUBE_READONLY_SCOPE",
     "YouTubeAccount",
     "YouTubeAdapter",
     "YouTubeApiClient",
     "YouTubeApiError",
+    "YouTubeDescriptionWriter",
     "YouTubeInventoryService",
+    "YouTubeRevisionConflictError",
+    "YouTubeWriteError",
+    "YouTubeWriteScopeError",
 ]
