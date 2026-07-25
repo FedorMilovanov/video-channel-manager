444031dad7c738661bff400b94631cb2de64a773
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-07-25 06:15:25 +0300

    Export local media quality gate

--- src/video_channel_manager/local_media/__init__.py
+++ src/video_channel_manager/local_media/__init__.py
@@ -1,3 +1,16 @@
+from video_channel_manager.local_media.quality import (
+    MediaQualityError,
+    MediaQualityReport,
+    probe_media,
+    sha256_file,
+)
 from video_channel_manager.local_media.scanner import LocalMediaRecord, scan_local_media
 
-__all__ = ["LocalMediaRecord", "scan_local_media"]
+__all__ = [
+    "LocalMediaRecord",
+    "MediaQualityError",
+    "MediaQualityReport",
+    "probe_media",
+    "scan_local_media",
+    "sha256_file",
+]
