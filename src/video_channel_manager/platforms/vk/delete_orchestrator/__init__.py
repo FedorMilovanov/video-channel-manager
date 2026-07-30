fb2b57447dc76f8ff0037980a1cd051e836f58f5
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-07-30 21:46:16 +0300

    Add durable VK delete orchestrator package

---
+++ src/video_channel_manager/platforms/vk/delete_orchestrator/__init__.py
@@ -0,0 +1,32 @@
+from video_channel_manager.platforms.vk.delete_orchestrator.evidence import DeleteEvidence
+from video_channel_manager.platforms.vk.delete_orchestrator.gateway import DeleteGateway, OwnerInventory, VkDeleteGateway
+from video_channel_manager.platforms.vk.delete_orchestrator.ledger import DeleteLedger
+from video_channel_manager.platforms.vk.delete_orchestrator.models import (
+    AttemptOutcome,
+    DeleteOperation,
+    DeletePolicy,
+    ExactVideoObservation,
+    OperationState,
+    OrchestratorConfig,
+    RunState,
+    VideoGuard,
+)
+from video_channel_manager.platforms.vk.delete_orchestrator.service import DeleteOrchestrator, ReconcileResult
+
+__all__ = [
+    "AttemptOutcome",
+    "DeleteEvidence",
+    "DeleteGateway",
+    "DeleteLedger",
+    "DeleteOperation",
+    "DeleteOrchestrator",
+    "DeletePolicy",
+    "ExactVideoObservation",
+    "OperationState",
+    "OrchestratorConfig",
+    "OwnerInventory",
+    "ReconcileResult",
+    "RunState",
+    "VideoGuard",
+    "VkDeleteGateway",
+]
