f5837e585e61e40630ec0e38ec496b8aa6798dfe
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-08-04 00:02:56 +0300

    Activate safe v5 upload retries

--- scripts/schedule_lord_god_article_wave_v3.py
+++ scripts/schedule_lord_god_article_wave_v3.py
@@ -7,6 +7,7 @@ import lord_god_article_wave_v3.common as common_module  # noqa: F401
 import lord_god_article_wave_v3.mutations as mutations_module  # noqa: F401
 import lord_god_article_wave_v3.photo_wave_v5 as photo_wave_module
 import lord_god_article_wave_v3.photo_wave_v5_compat as photo_wave_v5_compat  # noqa: F401
+import lord_god_article_wave_v3.photo_wave_v5_upload_retry as photo_wave_v5_upload_retry  # noqa: F401
 import lord_god_article_wave_v3.sources as sources_module  # noqa: F401
 import lord_god_article_wave_v3.wall as wall_module  # noqa: F401
 import lord_god_article_wave_v3.workflow as workflow_module  # noqa: F401
