d2e50a5ed0f7ccb65dc6112b2f02bdbd102883f1
FedorMilovanov <258225528+FedorMilovanov@users.noreply.github.com>
2026-08-20 03:50:42 +0300

    Instagram: add read-only provider identity package

---
+++ src/video_channel_manager/platforms/instagram/__init__.py
@@ -0,0 +1,6 @@
+from video_channel_manager.platforms.instagram.identity_client import (
+    InstagramFacebookIdentityClient,
+    InstagramIdentityReadError,
+)
+
+__all__ = ["InstagramFacebookIdentityClient", "InstagramIdentityReadError"]
